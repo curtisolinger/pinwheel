ec44154090b67650535e3e7fa1f91b7d5701294f
Curtis Olinger <curtisolinger@gmail.com>
2022-10-05 12:04:27 -0700

    Added comments to code

--- pinwheel.ino
+++ pinwheel.ino
@@ -1,19 +1,19 @@
-const int switchPin = 2;
-const int motorPin = 9;
+const int switchPin = 2;                  /* define the variable switchPin */
+const int motorPin = 9;                   /* define the variable motorPin */
 int switchState = 0;
 
 void setup() {
-  pinMode(motorPin, OUTPUT);
-  pinMode(switchPin, INPUT);
+  pinMode(motorPin, OUTPUT);              /* configure the digital pin 9 to be output */
+  pinMode(switchPin, INPUT);              /* configure the digital pin 2 to be input */
  }
 
 void loop() {
-  switchState = digitalRead(switchPin);
+  switchState = digitalRead(switchPin);   /* check the voltage level on pin 2 */
 
-  if (switchState == HIGH) {
-    digitalWrite(motorPin, HIGH);
+  if (switchState == HIGH) {              /* if digitalRead reads voltage on the switchPin, then */
+    digitalWrite(motorPin, HIGH);         /* send 5v to the motor */
   }
   else {
-    digitalWrite(motorPin, LOW);
+    digitalWrite(motorPin, LOW);          /* send 0v to the motor */
   }
 }
